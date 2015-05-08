58d0cddb7433fcd704d1547b5cb4a0ecb928f2c3
Matthew Pepers <pepers@users.noreply.github.com>
2015-05-08 16:32:25 -0400

    tab problems

--- src/Server.java
+++ src/Server.java
@@ -8,58 +8,59 @@ public class Server {
 	public static void main(String[] args) {
 		// create new thread to wait for and verify TFTP packets
 		Server c = new Server();
-	    c.receive();
+		c.receive();
 	}
 	
 	public Server() {
 		// create new socket to receive TFTP packets from Client
-	    try {         
-	    	receiveSocket = new DatagramSocket(69);	         
-	    } catch (SocketException se) {
-	    	se.printStackTrace();
-	        System.exit(1);
-	    }   
+		try {
+			receiveSocket = new DatagramSocket(69);
+		} catch (SocketException se) {
+			se.printStackTrace();
+			System.exit(1);
+		}   
 	}
 	
 	public void receive() {
 		while (true) {
 			// prepare for receiving packet
 			byte data[] = new byte[100];
-			receivePacket = new DatagramPacket(data, data.length);		      
-	    	System.out.println("Server: Waiting for Packet.\n");
+			receivePacket = new DatagramPacket(data, data.length);
+			System.out.println("Server: Waiting for Packet.\n");
 
-	    	// block until a datagram packet is received from receiveSocket
-	    	try {        
-	    		System.out.println("Waiting..."); // so we know we're waiting
-	    		receiveSocket.receive(receivePacket);
-	    	} catch (IOException e) {
-	    		System.out.print("IO Exception: likely:");
-	    		System.out.println("Receive Socket Timed Out.\n" + e);
-	    		e.printStackTrace();
-	    		System.exit(1);
-	    	}
-	    	
-	    	// check for valid TFTP request packet
-	    	if (data[1] == 1 && data[0] == 0) { 		// valid RRQ
-	    		System.out.println("\nServer: RRQ Received:");
-	    		
-	    		// create new thread to communicate with Client and transfer file
-		    	// pass it datagram that was received
+			// block until a datagram packet is received from receiveSocket
+			try {        
+				System.out.println("Waiting...");
+				receiveSocket.receive(receivePacket);
+			} catch (IOException e) {
+				System.out.print("IO Exception: likely:");
+				System.out.println("Receive Socket Timed Out.\n" + e);
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			// check for valid TFTP request packet
+			if (data[1] == 1 && data[0] == 0) { 		// valid RRQ
+				System.out.println("\nServer: RRQ Received:");
+					
+				// create new thread to communicate with Client and transfer file
+				// pass it datagram that was received
+				Thread clientConnectionThread = new Thread(new ClientConnection(data));
+				clientConnectionThread.start();
+					
+			} else if (data[1] == 2 && data[0] == 0) {	// valid WRQ
+				System.out.println("\nServer: WRQ Received:");
+				
+				// create new thread to communicate with Client and transfer file
+				// pass it datagram that was received
 				Thread clientConnectionThread = new Thread(new ClientConnection(data));
 				clientConnectionThread.start();
 				
-	    	} else if (data[1] == 2 && data[0] == 0) {	// valid WRQ
-	    		System.out.println("\nServer: WRQ Received:");
-	    		
-	    		// create new thread to communicate with Client and transfer file
-		    	// pass it datagram that was received
-				Thread clientConnectionThread = new Thread(new ClientConnection(data));
-				clientConnectionThread.start();
+			} else {					// invalid packet
+				System.out.println("\nServer: Invalid Request Packet Received:");
 				
-	    	} else {									// invalid packet
-	    		System.out.println("\nServer: Invalid Request Packet Received:");
-	    	} 
-		}
+			} 
+		} // end of while
 	}
 }
 
