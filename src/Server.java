7a021a6c34096fdf9134810d6c813beb9dc1fa61
pepers <matt.pepers@gmail.com>
2015-05-08 22:34:39 -0400

    more opcode stuffs

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,12 @@
 import java.io.IOException;
 import java.net.*;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 
-public class Server {
+public class Server {	
+	// requests we can receive
+	public static enum Opcode {RRQ, WRQ, INVALID}; 
+	
 	DatagramPacket receivePacket;
 	DatagramSocket receiveSocket;
 
@@ -40,49 +45,72 @@ public class Server {
 			}
 			
 			// check for valid TFTP request packet
+			Opcode op;
 			if (data[0] != 0) { // invalid Opcode
-				System.out.println("\nServer: Invalid Opcode Received:");
+				System.out.println("Server: Invalid Opcode Received: \n");
+				op = Opcode.INVALID;
 				break;
 			} else {
-				byte op = data[1];
-				switch (op) {
-					// valid request
-					case 1: case 2:
-						System.out.println("\nServer: Valid Reqest Received:");					
-						// create new thread to communicate with Client and transfer file
-						// pass it datagram that was received
-						Thread clientConnectionThread = new Thread(
-								new ClientConnection(receivePacket));
-						clientConnectionThread.start();	
-						System.out.println("\nServer: Packet Sent for Processing:");
+				switch (data[1]) {
+					case 1: 		
+						op = Opcode.RRQ;
 						break;
-					// invalid request
-					default:
-						System.out.println("\nServer: Invalid Request Received:");
+					case 2:
+						op = Opcode.WRQ;
+						break;
+					default: // invalid reqest
+						System.out.println("Server: Invalid Request Received: \n");
+						op = Opcode.INVALID;
 						break;
 				}
+			}
+			
+			// pass valid request datagram
+			if (op != Opcode.INVALID) {
+				System.out.println("Server: Valid Reqest Received: \n");					
+				// create new thread to communicate with Client and transfer file
+				// pass it datagram that was received
+				Thread clientConnectionThread = new Thread(
+						new ClientConnection(receivePacket, op));
+				clientConnectionThread.start();	
+				System.out.println("Server: Packet Sent for Processing: \n");
 			}
 		} // end of while
 	}
 }
 
 class ClientConnection implements Runnable {
-	// requests we can receive
-	public static enum Opcode {RRQ, WRQ}; 
+	Server.Opcode op;
 	
 	DatagramPacket receivePacket;
 	DatagramPacket sendPacket;
-	DatagramSocket sendSocket;
-	
+	DatagramSocket sendSocket;	
 
-	public ClientConnection(DatagramPacket receivePacket) {
+	public ClientConnection(DatagramPacket receivePacket, Server.Opcode opServer) {
 		// pass in the received datagram packet from the Server
-		// in order to facilitate file trasfers with the Client
+		// in order to facilitate file transfers with the Client
 		this.receivePacket = receivePacket;
+		this.op = opServer;
 	}
 	
 	public void run() {
-		// TODO Auto-generated method stub
+  	  // to get rid of trailing null bytes from buffer	      
+  	  byte received[] = new byte[receivePacket.getLength()]; 
+  	  System.arraycopy(receivePacket, 0, received, 0, receivePacket.getLength()); 
+	  
+  	  // deal with OPcodes
+  	  if (op == Server.Opcode.RRQ) {
+  		  System.out.println("Server: Read Request Received:");
+  	  } else if (op == Server.Opcode.WRQ) {
+  		  System.out.println("Server: Write Request Received:");
+  	  }
+  	  
+  	  // process the received datagram and print data
+  	  System.out.println("From host: " + receivePacket.getAddress() + " : " + receivePacket.getPort());
+  	  System.out.print("Containing " + receivePacket.getLength() + " bytes: \n");
+  	  System.out.println(Arrays.toString(received));
+  	  System.out.print("\tFilename: " + new String(receivePacket.getData(), 2, 9, Charset.forName("utf-8")) +
+			  			"\t\tMode: " + new String(receivePacket.getData(), 12, 5, Charset.forName("utf-8")) + "\n");
 		
 	}	
 }
